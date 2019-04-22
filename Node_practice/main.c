0690ac565b77658ebe44992fc57f28bb428edfcd
차승주 <chaseungju@chaseungjuui-MacBook-Pro.local>
2019-04-22 22:19:17 +0900

    comple

--- Node_practice/main.c
+++ Node_practice/main.c
@@ -2,14 +2,52 @@
 //  main.c
 //  Node_practice
 //
+//  algorithm and data structure p.142
+//  program : Node
+//
 //  Created by 차승주 on 2019. 4. 22..
 //  Copyright © 2019년 cha. All rights reserved.
+//  Created dOn MacBook Pro 2017
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct NODE{
+    struct NODE *next;
+    int data;
+} node;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int i=0;
+    node * first = NULL;
+    first = malloc(sizeof(node));
+    
+    node * node1 = malloc(sizeof(node));
+    first->next = node1;
+    node1->data = 10;
+    
+    node * node2 = malloc(sizeof(node));
+    node1->next = node2;
+    node2->data = 40;
+    
+    node * node3 = malloc(sizeof(node));
+    node2->next = node3;
+    node3->data = 100;
+    node3->next = NULL;
+    
+    node * cur = first->next;
+    
+    while(cur != NULL){
+        printf("node %d : %d\n\n", i+1, cur->data);
+        cur = cur->next;
+        i+=1;
+    }
+    
+    free(node3);
+    free(node2);
+    free(node1);
+    free(first);
+    
     return 0;
 }
